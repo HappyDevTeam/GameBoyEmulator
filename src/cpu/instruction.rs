aaf28e92cfb09776cb6c68ef9851faa202f5370c
chansoen <22986484+chsoen@users.noreply.github.com>
2025-05-11 21:52:45 -0700

    feat: test bit execution arms.

--- src/cpu/instruction.rs
+++ src/cpu/instruction.rs
@@ -362,6 +362,72 @@ impl CPU {
                 2
             }
             0x2f => { self.registers.a = self.shift_right_arithmetic(self.registers.a); 2 }
+            
+            // BIT
+            0x40 => { self.test_bit(self.registers.b,                            0); 2 }
+            0x41 => { self.test_bit(self.registers.c,                            0); 2 }
+            0x42 => { self.test_bit(self.registers.d,                            0); 2 }
+            0x43 => { self.test_bit(self.registers.e,                            0); 2 }
+            0x44 => { self.test_bit(self.registers.h,                            0); 2 }
+            0x45 => { self.test_bit(self.registers.l,                            0); 2 }
+            0x46 => { self.test_bit(self.bus.read_byte(self.registers.get_hl()), 0); 2 }
+            0x47 => { self.test_bit(self.registers.a,                            0); 2 }
+            0x48 => { self.test_bit(self.registers.b,                            1); 2 }
+            0x49 => { self.test_bit(self.registers.c,                            1); 2 }
+            0x4a => { self.test_bit(self.registers.d,                            1); 2 }
+            0x4b => { self.test_bit(self.registers.e,                            1); 2 }
+            0x4c => { self.test_bit(self.registers.h,                            1); 2 }
+            0x4d => { self.test_bit(self.registers.l,                            1); 2 }
+            0x4e => { self.test_bit(self.bus.read_byte(self.registers.get_hl()), 1); 2 }
+            0x4f => { self.test_bit(self.registers.a,                            1); 2 }
+            0x50 => { self.test_bit(self.registers.b,                            2); 2 }
+            0x51 => { self.test_bit(self.registers.c,                            2); 2 }
+            0x52 => { self.test_bit(self.registers.d,                            2); 2 }
+            0x53 => { self.test_bit(self.registers.e,                            2); 2 }
+            0x54 => { self.test_bit(self.registers.h,                            2); 2 }
+            0x55 => { self.test_bit(self.registers.l,                            2); 2 }
+            0x56 => { self.test_bit(self.bus.read_byte(self.registers.get_hl()), 2); 2 }
+            0x57 => { self.test_bit(self.registers.a,                            2); 2 }
+            0x58 => { self.test_bit(self.registers.b,                            3); 2 }
+            0x59 => { self.test_bit(self.registers.c,                            3); 2 }
+            0x5a => { self.test_bit(self.registers.d,                            3); 2 }
+            0x5b => { self.test_bit(self.registers.e,                            3); 2 }
+            0x5c => { self.test_bit(self.registers.h,                            3); 2 }
+            0x5d => { self.test_bit(self.registers.l,                            3); 2 }
+            0x5e => { self.test_bit(self.bus.read_byte(self.registers.get_hl()), 3); 2 }
+            0x5f => { self.test_bit(self.registers.a,                            3); 2 }
+            0x60 => { self.test_bit(self.registers.b,                            4); 2 }
+            0x61 => { self.test_bit(self.registers.c,                            4); 2 }
+            0x62 => { self.test_bit(self.registers.d,                            4); 2 }
+            0x63 => { self.test_bit(self.registers.e,                            4); 2 }
+            0x64 => { self.test_bit(self.registers.h,                            4); 2 }
+            0x65 => { self.test_bit(self.registers.l,                            4); 2 }
+            0x66 => { self.test_bit(self.bus.read_byte(self.registers.get_hl()), 4); 2 }
+            0x67 => { self.test_bit(self.registers.a,                            4); 2 }
+            0x68 => { self.test_bit(self.registers.b,                            5); 2 }
+            0x69 => { self.test_bit(self.registers.c,                            5); 2 }
+            0x6a => { self.test_bit(self.registers.d,                            5); 2 }
+            0x6b => { self.test_bit(self.registers.e,                            5); 2 }
+            0x6c => { self.test_bit(self.registers.h,                            5); 2 }
+            0x6d => { self.test_bit(self.registers.l,                            5); 2 }
+            0x6e => { self.test_bit(self.bus.read_byte(self.registers.get_hl()), 5); 2 }
+            0x6f => { self.test_bit(self.registers.a,                            5); 2 }
+            0x70 => { self.test_bit(self.registers.b,                            6); 2 }
+            0x71 => { self.test_bit(self.registers.c,                            6); 2 }
+            0x72 => { self.test_bit(self.registers.d,                            6); 2 }
+            0x73 => { self.test_bit(self.registers.e,                            6); 2 }
+            0x74 => { self.test_bit(self.registers.h,                            6); 2 }
+            0x75 => { self.test_bit(self.registers.l,                            6); 2 }
+            0x76 => { self.test_bit(self.bus.read_byte(self.registers.get_hl()), 6); 2 }
+            0x77 => { self.test_bit(self.registers.a,                            6); 2 }
+            0x78 => { self.test_bit(self.registers.b,                            7); 2 }
+            0x79 => { self.test_bit(self.registers.c,                            7); 2 }
+            0x7a => { self.test_bit(self.registers.d,                            7); 2 }
+            0x7b => { self.test_bit(self.registers.e,                            7); 2 }
+            0x7c => { self.test_bit(self.registers.h,                            7); 2 }
+            0x7d => { self.test_bit(self.registers.l,                            7); 2 }
+            0x7e => { self.test_bit(self.bus.read_byte(self.registers.get_hl()), 7); 2 }
+            0x7f => { self.test_bit(self.registers.a,                            7); 2 }
 
             _ => panic!("Unknown instruction found for: 0x{:x}", byte),
         }
